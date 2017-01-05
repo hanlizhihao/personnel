1ac382931c823165de38b4e9fce72522558b94b3
韩励智 <hanlizhi@hotmail.com>
2017-01-05 13:32:53 +0800

    Began to encapsulate the DAO

---
+++ src/main/java/com/xuchengguo/personnel/entity/Announcement.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xuchengguo.personnel.entity;
+
+import java.sql.Date;
+
+/**
+ *检务公告类
+ * @author Administrator
+ */
+public class Announcement {
+    private int id;
+    private String title;//标题
+    private Date time;
+}
